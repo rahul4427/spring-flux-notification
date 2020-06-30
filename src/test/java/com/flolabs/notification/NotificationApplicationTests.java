7d0d16295bbe5b1cef0132615564cb4aeeb0d9d1
Rahul Kumar <rahul.rk0627@gmail.com>
2020-06-30 20:02:02 +0530

    first commit

---
+++ src/test/java/com/flolabs/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flolabs.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
